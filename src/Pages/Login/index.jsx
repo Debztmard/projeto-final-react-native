58eeda1e3900d3fb1fbed3f28b612d504ee3af43
camillaslp <slpcamilla@gmail.com>
2021-12-11 17:31:54 -0300

    Login inicial

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,11 +1,33 @@
-import React from "react";
-import { Text, View } from "react-native";
 import { styles } from "./styles";
+import React, { useState } from "react";
+import { Button, TextInput, View } from "react-native";
+
 
 export default function Login({ navigation }) {
+  const [nome, setNome] = useState(null);
+  const [senha, setsenha] = useState(null);
+
+  
+ const handleClick = () => {
+    if (nome === null || senha === null) return;
+
+  };
+
   return (
     <View style={styles.container}>
-      <Text>LOGIN</Text>
+      <TextInput
+        style={{ width: "80%", borderWidth: 1, padding: 10, marginBottom: 10 }}
+        placeholder="Digite eu Nome"
+        value={nome}
+        onChangeText={setNome}
+      />
+      <TextInput
+        style={{ width: "80%", borderWidth: 1, padding: 10, marginBottom: 10 }}
+        placeholder="Digite sua senha"
+        value={senha}
+        onChangeText={setsenha}
+      />
+      <Button title="Entrar" onPress={handleClick} />
     </View>
   );
 }
--- src/Pages/Login/styles.js
+++ src/Pages/Login/styles.js
@@ -6,6 +6,7 @@ export const styles = StyleSheet.create({
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
+      marginTop: 50,
     },
   });
   
