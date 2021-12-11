17eb02fdfe9b89355c271ffde14da0b4a4cedb6b
camillaslp <slpcamilla@gmail.com>
2021-12-11 12:54:45 -0300

    Camilla altera App

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import Home from "./src/pages/Home";
-import Login from "./src/pages/Login";
+import Home from "./src/Pages/Home";
+import Login from "./src/Pages/Login";
 import Galeria from "./src/Pages/Galeria";
 import Carrinho from "./src/Pages/Carrinho";
 import Detalhes from "./src/Pages/Detalhes";
